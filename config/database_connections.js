5ffa3577d1c0e06449d4fdfbfa3303b5ad78058d
scaradook13 <mythicalwarrior132006@gmail.com>
2025-11-05 16:38:20 +0800

    change on db

--- config/database_connections.js
+++ config/database_connections.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose
-  // .connect(process.env.MONGO_DB_ONLINE, {})
-  .connect(process.env.MONGO_DB_LOCAL, {})
+  .connect(process.env.MONGO_DB_ONLINE, {})
+  // .connect(process.env.MONGO_DB_LOCAL, {})
   .then(() => console.log("Connected to Database"))
   .catch((err) => console.error("Error connecting to Database", err));
