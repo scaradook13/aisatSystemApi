f4ba7efbe6b8b1551a07968b521eec5db275855d
scaradook13 <mythicalwarrior132006@gmail.com>
2025-11-09 10:47:00 +0800

    FOR VERCEL API

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/v1/admin", adminRoutes);
 
 module.exports = app;
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
